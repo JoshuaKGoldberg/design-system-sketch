46cb2824beaf99b54d29c1a10109e1d056417d91
Josh Goldberg <git@joshuakgoldberg.com>
2023-01-16 13:14:51 -0600

    fix: undid after changes in before files ✨

--- before.jsx
+++ before.jsx
@@ -2,18 +2,27 @@ import styles from "./before.module.css";
 
 export function Home() {
   return (
-    <div className={styles.theme}>
-      {/* eslint-disable-next-line no-header-v1 */}
-      <HeaderV1 />
-      <MainContent>
-        <Text as="h1">Welcome to Contoso</Text>
-        <Text as="p">Lorem ipsum...</Text>
-        <Text as="h2">Second Heading</Text>
-        <Text as="p">Lorem ipsum...</Text>
-        <Text as="h2">Third Heading</Text>
-        <Text as="p">Lorem ipsum...</Text>
-      </MainContent>
-      <Footer />
+    <div>
+      <header className={styles.header}>
+        <a href="/">
+          <img alt="Contoso logo" className={styles.logo} src="/logo.svg" />
+        </a>
+      </header>
+      <main>
+        <div className={styles.mainInside}>
+          <h1 className={styles.h1}>Welcome to Contoso</h1>
+          <p className={styles.p}>Lorem ipsum...</p>
+          <h2 className={styles.h2}>Second Heading</h2>
+          <p className={styles.p}>Lorem ipsum...</p>
+          <h2 className={styles.h2}>Third Heading</h2>
+          <p className={styles.p}>Lorem ipsum...</p>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a href="/">
+          <img alt="Contoso logo" className={styles.logo} src="/logo.svg" />
+        </a>
+      </footer>
     </div>
   );
 }
