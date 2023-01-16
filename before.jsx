b07189f4f5bdebe0d7816a789ea21d3206bcfaf0
Josh Goldberg <git@joshuakgoldberg.com>
2023-01-16 10:10:21 -0600

    Initial commit ✨

---
+++ before.jsx
@@ -0,0 +1,28 @@
+import styles from "./before.module.css";
+
+export function Home() {
+  return (
+    <div>
+      <header className={styles.header}>
+        <a href="/">
+          <img alt="Contoso logo" className={styles.logo} src="/logo.svg" />
+        </a>
+      </header>
+      <main>
+        <div className={styles.mainInside}>
+          <h1 className={styles.h1}>Welcome to Contoso</h1>
+          <p className={styles.p}>Lorem ipsum...</p>
+          <h2 className={styles.h2}>Second Heading</h2>
+          <p className={styles.p}>Lorem ipsum...</p>
+          <h2 className={styles.h2}>Third Heading</h2>
+          <p className={styles.p}>Lorem ipsum...</p>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a href="/">
+          <img alt="Contoso logo" className={styles.logo} src="/logo.svg" />
+        </a>
+      </footer>
+    </div>
+  );
+}
